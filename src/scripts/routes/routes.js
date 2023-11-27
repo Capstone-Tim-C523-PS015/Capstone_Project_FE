4192941087be975aee6508d8d79f2580543f3ca2
Aang Anggriawan <aanganggriawan222@gmail.com>
2023-11-27 09:54:54 +0700

    Membuat feature Dashboar Calendar

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import About from "../../views/page/about";
 import Home from "../../views/page/home";
 import Kalendar from "../../views/page/dashboard/calendar";
 import TodoList from "../../views/page/dashboard/todoList";
+import Histori from "../../views/page/dashboard/histori";
 const routes = {
   "/": Home,
   "/home": Home,
   "/about": About,
   "/calendar": Kalendar,
   "/todolist": TodoList,
+  "/histori": Histori,
 };
 
 export default routes;
--- src/views/page/dashboard/todoList.js
+++ src/views/page/dashboard/todoList.js
@@ -1,7 +1,12 @@
 const TodoList = {
   async render() {
     return `
-          <h1>Hallo Todo List</h1>
+          <di>
+            <h1 class="border-black border-b-2">HARI INI</h1>
+            <div>
+
+            </div>
+          </div>
           `;
   },
 
