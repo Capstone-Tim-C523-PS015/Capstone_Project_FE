cf711a617fab3881a0a735d54613c34697ec7d41
Aang Anggriawan <aanganggriawan222@gmail.com>
2023-12-10 18:46:45 +0700

    Menambahkan PWA Manifest dan Service Worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,79 +1,81 @@
 /* eslint-disable import/order */
 /* eslint-disable no-unused-vars */
-import 'flowbite';
-import '../styles/index.css';
-import App from '../views/app';
-import 'fullcalendar';
-import { createElement } from '@fullcalendar/core/preact';
+import "flowbite";
+import "../styles/index.css";
+import App from "../views/app";
+import "fullcalendar";
+import { createElement } from "@fullcalendar/core/preact";
+import swRegister from "./utils/sw-register";
 
-const app = new App({ content: document.querySelector('.content') });
-window.addEventListener('hashchange', () => {
+const app = new App({ content: document.querySelector(".content") });
+window.addEventListener("hashchange", () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
+  swRegister();
 });
 
 const dashboard = new App({
-  content: document.querySelector('.contentDashboard'),
+  content: document.querySelector(".contentDashboard"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   dashboard.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   dashboard.renderPage();
 });
 const indexdashboard = new App({
-  content: document.querySelector('.contentindexdash'),
+  content: document.querySelector(".contentindexdash"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   indexdashboard.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   indexdashboard.renderPage();
 });
 const contentlogin = new App({
-  content: document.querySelector('.contentuser'),
+  content: document.querySelector(".contentuser"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   contentlogin.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   contentlogin.renderPage();
 });
 const contentchangepassword = new App({
-  content: document.querySelector('.forgotpassword'),
+  content: document.querySelector(".forgotpassword"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   contentchangepassword.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   contentchangepassword.renderPage();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Ambil semua elemen dengan kelas 'menu-item'
-  const menuItems = document.querySelectorAll('.menu-item');
+  const menuItems = document.querySelectorAll(".menu-item");
 
   // Tambahkan event listener untuk setiap elemen menu
   menuItems.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
       // Hapus kelas 'active' dari semua elemen menu
       menuItems.forEach((menuItem) => {
-        menuItem.classList.remove('active');
+        menuItem.classList.remove("active");
       });
 
       // Tambahkan kelas 'active' ke elemen menu yang diklik
-      item.classList.add('active');
+      item.classList.add("active");
     });
   });
 });
--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
+    app: path.resolve(__dirname, "src/scripts/index.js"),
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
@@ -20,13 +21,13 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
           },
         ],
       },
@@ -34,44 +35,54 @@ module.exports = {
   },
   plugins: [
     new FaviconsWebpackPlugin({
-      logo: './src/public/icons/icon.jpg',
+      logo: "./src/public/icons/icon.jpg",
     }),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/views/index.html'),
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/views/index.html"),
     }),
     new HtmlWebpackPlugin({
-      filename: 'profil.html',
-      template: path.resolve(__dirname, 'src/views/admin/profil.html'),
+      filename: "profil.html",
+      template: path.resolve(__dirname, "src/views/admin/profil.html"),
     }),
     new HtmlWebpackPlugin({
-      filename: 'login.html',
-      template: path.resolve(__dirname, 'src/views/admin/login.html'),
+      filename: "login.html",
+      template: path.resolve(__dirname, "src/views/admin/login.html"),
     }),
     new HtmlWebpackPlugin({
-      filename: 'dashboard.html',
-      template: path.resolve(__dirname, 'src/views/page/dashboard/dashboard.html'),
+      filename: "dashboard.html",
+      template: path.resolve(
+        __dirname,
+        "src/views/page/dashboard/dashboard.html"
+      ),
     }),
     new HtmlWebpackPlugin({
-      filename: 'indexdash.html',
-      template: path.resolve(__dirname, 'src/views/page/dashboard/indexdash.html'),
+      filename: "indexdash.html",
+      template: path.resolve(
+        __dirname,
+        "src/views/page/dashboard/indexdash.html"
+      ),
     }),
     new HtmlWebpackPlugin({
-      filename: 'lupapassword.html',
-      template: path.resolve(__dirname, 'src/views/admin/lupapassword.html'),
+      filename: "lupapassword.html",
+      template: path.resolve(__dirname, "src/views/admin/lupapassword.html"),
     }),
     new HtmlWebpackPlugin({
-      filename: 'recovery.html',
-      template: path.resolve(__dirname, 'src/views/admin/recovery.html'),
+      filename: "recovery.html",
+      template: path.resolve(__dirname, "src/views/admin/recovery.html"),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, "src/public/"),
+          to: path.resolve(__dirname, "dist/"),
         },
       ],
     }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: "./sw.bundle.js",
+    }),
+
     new CleanWebpackPlugin(),
   ],
 };
